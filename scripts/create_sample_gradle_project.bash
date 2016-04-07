8caf0f7aea1100c88289bd466d960f7d42d8c746
Laurent Krishnathas <lkbis2009@googlemail.com>
2016-04-07 14:59:59 +0100

    bk

--- scripts/create_sample_gradle_project.bash
+++ scripts/create_sample_gradle_project.bash
@@ -1,5 +1,14 @@
 gradle init --type groovy-library
-mkdir -p src/main/java src/main/resources  src/test/java src/test/resources src/main/webapp
-touch gradle.properties
+mkdir -p src/main/java 
+mkdir -p src/main/resources
+mkdir -p src/test/java 
+mkdir -p src/test/resources 
+mkdir -p src/main/webapp
 
-echo Done.
+mkdir -p buildSrc/src/main/java 
+mkdir -p buildSrc/src/main/groovy 
+mkdir -p buildSrc/src/main/resources  
+mkdir -p buildSrc/src/test/java 
+mkdir -p buildSrc/src/test/groovy
+mkdir -p buildSrc/src/test/resources 
+
